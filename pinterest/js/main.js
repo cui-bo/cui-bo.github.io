885b6afa4168a8ace297b6c2ec0c131b35669f78
cupidcb <bo.cui@supinfo.com>
2014-12-13 12:44:03 +0100

    Change index.html

---
+++ pinterest/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(
+  function(){
+    // Fill html
+    $.getJSON("pinterest/data/site.json", function(data){
+        console.log(data.sites[0].site_name);
+        var imgPath = 'pinterest/img/';
+        $.each(data.sites, function(index, site){
+            var aHtml = '<div class="pin">' +
+                        '<h3>' + site.site_name + '</h3>' +
+                        '<a href="' + site.site_url_git + '">' +
+                        '<img src="' + imgPath + site.img_name + '" />' +
+                        '<p>' + site.site_desc + '</p></a></div>';
+            $(aHtml).appendTo("#columns");
+        });
+    });
+  }
+);
