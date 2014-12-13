182747babf937d9d619203af1b12f88061dbb026
cupidcb <bo.cui@supinfo.com>
2014-12-13 10:50:30 +0100

    update index

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<head>
+  <title>List all sites</title>
+
+  <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+  <div id="wrapper">
+    <div id="columns">
+      <div class="pin">
+        <a href="http://jspractice.com/alias/">
+          <img src="img/alias.png" />
+          <p>
+            A demo site. Original idea from ALIAS.
+          </p>
+        </a>
+      </div>
+
+      <div class="pin">
+        <a href="http://jspractice.com/puzzle/">
+          <img src="img/puzzle.png" />
+          <p>
+            A site of puzzle.
+          </p>
+        </a>
+      </div>
+
+      <div class="pin">
+        <a href="http://jspractice.com/sudoku/">
+          <img src="img/sudoku.png" />
+          <p>
+            Sudoku solver !
+          </p>
+        </a>
+      </div>
+
+    </div>
+  </div>
+</body>
+</html>
