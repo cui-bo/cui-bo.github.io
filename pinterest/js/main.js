54fdb9aceaa9bc49ff9ae8fe26824bf88a1043cb
cupidcb <bo.cui@supinfo.com>
2014-12-25 23:39:10 +0100

    update pinterest image path

--- pinterest/js/main.js
+++ pinterest/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(
     // Fill html
     $.getJSON("data/site.json", function(data) {
       console.log(data.sites[0].site_name);
-      var imgPath = 'pinterest/img/';
+      var imgPath = 'img/';
       $.each(data.sites, function(index, site) {
         var aHtml = '<div class="pin">' +
           '<h3>' + site.site_name + '</h3>' +
