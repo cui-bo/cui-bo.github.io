6c3591fdcd2d5d6f82c1fd9b32ab3c20ed59f06b
cupidcb <bo.cui@supinfo.com>
2015-01-18 00:12:46 +0100

    merge login js

--- js/login/main.js
+++ js/login/main.js
@@ -1,41 +1,41 @@
 $(document).ready(function() {
- var arrUsers = [];
-  // Get users
-   $.ajax({
-   	url: "_data/users.json",
-   	async: false,
-   	dataType: 'json',
-   	success: function(data) {
-   		$.each(data.users, function(key, user) {
-   			arrUsers.push(user);
-   		});
-   	}
-   });
+	var arrUsers = [];
+	// Get users
+	$.ajax({
+		url: "_data/users.json",
+		async: false,
+		dataType: 'json',
+		success: function(data) {
+			$.each(data.users, function(key, user) {
+				arrUsers.push(user);
+			});
+		}
+	});
 
-  var validLogin = function() {
-    var inputUsername = $('input[name="login"]').val();
-    var inputPassword = $('input[name="password"]').val();
-    // Redirect
-    $.each(arrUsers, function(key, user) {
-      if ((user.username == inputUsername) && (user.password ==
-          inputPassword)) {
-        //window.location.replace('pinterest/index.html');
-        window.location.replace('http://cuiborails.wordpress.com');
-      }
-    });
-  }
+	var validLogin = function() {
+		var inputUsername = $('input[name="login"]').val();
+		var inputPassword = $('input[name="password"]').val();
+		// Redirect
+		$.each(arrUsers, function(key, user) {
+			if ((user.username == inputUsername) && (user.password ==
+					inputPassword)) {
+				//window.location.replace('pinterest/index.html');
+				window.location.replace('http://cuiborails.wordpress.com');
+			}
+		});
+	}
 
-  // Click submit button
-  $('#submit').click(function() {
-    validLogin();
-  });
+	// Click submit button
+	$('#submit').click(function() {
+		validLogin();
+	});
 
-  // Press enter key
-  $('input[name="password"]').keydown(function(event) {
-    if (event.keyCode == 13) {
-      validLogin();
-    }
-  });
+	// Press enter key
+	$('input[name="password"]').keydown(function(event) {
+		if (event.keyCode == 13) {
+			validLogin();
+		}
+	});
 
 
 });
