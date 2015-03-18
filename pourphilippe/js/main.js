65b3c1285ea7d205edf72e10605cf600e7ba9369
Bo <cupidcb@gmail.com>
2015-03-18 16:33:17 +0100

    add control search datatable

--- pourphilippe/js/main.js
+++ pourphilippe/js/main.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-    $('#datatable').dataTable({
+    var table = $('#datatable').DataTable({
     	paging: false,
-    	searching: false,
+    	searching: true,
     	"info": false
     });
+	
+	$("#recherche").on( 'keyup', function() {
+	console.log(this.value);
+		table.search( this.value ).draw();
+	});
 } );
