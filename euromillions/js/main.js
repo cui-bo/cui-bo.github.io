fcd69e3dd3b4929bb33efe7160e76698b3418124
cui-bo <cupidcb@gmail.com>
2015-12-07 15:41:02 +0100

    replace PHP by javascript

--- euromillions/js/main.js
+++ euromillions/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-
+/*
   $('#chiffre').click(function() {
         $.ajax({
             type: 'POST',
@@ -34,4 +34,38 @@ $(document).ready(function(){
               },
           });
       });
+
+      */
+
+      function getRandomNumber(range, limit) {
+        var arr = []
+        while(arr.length < limit){
+          var randomnumber=Math.ceil(Math.random()*range)
+          var found=false;
+          for(var i=0;i<arr.length;i++){
+        	if(arr[i]==randomnumber){found=true;break}
+          }
+          if(!found)arr[arr.length]=randomnumber;
+        }
+        return arr;
+      }
+
+
+      $('#chiffre').click(function() {
+            var result = getRandomNumber(50, 5);
+            var ball = '';
+            for (var i = 0; i < result.length; i++) {
+              ball = ball.concat('<li class="ball">' + result[i] + '</li>');
+            }
+            $(".balls").html(ball);
+        });
+
+        $('#etoile').click(function() {
+              var result = getRandomNumber(10, 2);
+              var star = '';
+              for (var i = 0; i < result.length; i++) {
+                star = star.concat('<li class="star">' + result[i] + '</li>');
+              }
+              $(".stars").html(star);
+          });
 });
